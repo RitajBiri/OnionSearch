0d01cb6e9f7b822d4ccc6aaa6c8dab247f54f649
Ritaj Biri <77435663+RitajBiri@users.noreply.github.com>
2024-05-16 20:03:57 +0200

    Update core.py

--- onionsearch/core.py
+++ onionsearch/core.py
@@ -21,22 +21,22 @@ from urllib3.exceptions import ProtocolError
 
 ENGINES = {
     "ahmia": "http://juhanurmihxlp77nkq76byazcldy2hlmovfu2epvl5ankdibsot4csyd.onion",  # Offline?
-    "darksearchio": "http://darksearch.io",
+   # "darksearchio": "http://darksearch.io",
     "onionland": "http://3bbad7fauom4d6sgppalyqddsqbf5u5p56b5k5uk2zxsy3d6ey2jobad.onion",
-    "notevil": "http://hss3uro2hsxfogfq.onion",  # Offline?
+   # "notevil": "http://hss3uro2hsxfogfq.onion",  # Offline?
     "darksearchenginer": "http://l4rsciqnpzdndt2llgjx3luvnxip7vbyj6k6nmdy4xs77tx6gkd24ead.onion",
     "phobos": "http://phobosxilamwcg75xt22id7aywkzol6q6rfl2flipcqoc4e4ahima5id.onion",
     "onionsearchserver": "http://3fzh7yuupdfyjhwt3ugzqqof6ulbcl27ecev33knxe3u7goi3vfn2qqd.onion",
     "torgle": "http://no6m4wzdexe3auiupv2zwif7rm6qwxcyhslkcnzisxgeiw6pvjsgafad.onion",  # "torgle" -> "Submarine"
-    "torgle1": "http://torgle5fj664v7pf.onion",  # Offline?
-    "onionsearchengine": "http://onionf4j3fwqpeo5.onion",  # Offline?
-    "tordex": "http://tordex7iie7z2wcg.onion",  # Offline?
+  #  "torgle1": "http://torgle5fj664v7pf.onion",  # Offline?
+   # "onionsearchengine": "http://onionf4j3fwqpeo5.onion",  # Offline?
+   # "tordex": "http://tordex7iie7z2wcg.onion",  # Offline?
     "tor66": "http://tor66sewebgixwhcqfnp5inzp5x5uohhdy3kvtnyfxc2e5mxiuh34iid.onion",
     "tormax": "http://tormaxunodsbvtgo.onion",  # Offline?
     "haystack": "http://haystak5njsmn2hqkewecpaxetahtwhsbsa64jom2k22z5afxhnpxfid.onion",
-    "multivac": "http://multivacigqzqqon.onion",  # Offline?
-    "evosearch": "http://evo7no6twwwrm63c.onion",  # Offline?
-    "deeplink": "http://deeplinkdeatbml7.onion",  # Offline?
+#    "multivac": "http://multivacigqzqqon.onion",  # Offline?
+ #   "evosearch": "http://evo7no6twwwrm63c.onion",  # Offline?
+  #  "deeplink": "http://deeplinkdeatbml7.onion",  # Offline?
 }
 
 desktop_agents = [
